101fc2b476bd5a23b81e493aa0fc01b0cec4e00c
Alejandro <apachecom91@gmail.com>
2020-07-06 11:34:17 -0400

    config file

--- config.sh
+++ config.sh
@@ -3,3 +3,24 @@ echo building repair integer version
 cd external/repair
 make
 cd ../../
+
+mkdir cmake-build-release
+cd cmake-build-release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+echo building tests
+
+make t_hash_function
+make t_hash_sliding_windows
+make t_hash_parser
+make t_recursive_repair
+
+
+./t_hash_function
+./t_hash_sliding_windows
+./t_hash_parser
+./t_recursive_repair
+
+echo building binnary
+
+make rrepair
+./rrepair
