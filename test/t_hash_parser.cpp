d7ef6f3687bde9855a93e0e44f8ac13d8c75748a
Alejandro Pacheco <apachecom91@gmail.com>
2020-11-30 16:15:21 -0300

    ok

--- test/t_hash_parser.cpp
+++ test/t_hash_parser.cpp
@@ -10,12 +10,10 @@
 #include "../HashParserConfig.h"
 #include "../RepairUtils.h"
 
-#define LEN 100000000
+#define LEN 100000
 using namespace big_repair;
 using namespace std;
-std::map<uint,std::string> dir = {
-        {0,"/d1/apacheco/collections/big-repair/chr/uncompress_col/chr19x50.fa.out"}
-};
+
 //
 //static void tParseFile(benchmark::State & state)
 //{
@@ -41,114 +39,45 @@ std::map<uint,std::string> dir = {
 //
 //}
 
-static void tParseFileSMBigFile(benchmark::State & state)
-{
-    uint64_t coll = state.range(0);
-    // Perform setup here
-    for (auto _ : state) {
-        // This code gets timed
-        HashParserConfig<KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string>> conf(10,1,200,dir[coll],"./");
-        conf.print();
-        HashParser< HashParserConfig< KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string> >> parser(conf);
-            try {
-                parser.parseFileSM();
-                parser.results.print();
-                parser.recreateFile("tmp",1);
-            } catch (const char * s) {
-                std::cout<<s<<std::endl;
-            }
-
-
-        EXPECT_TRUE(util::compareFiles("tmp","tmp_recreated"));
-    }
-
-
-}
-
-static void tParseFileSM(benchmark::State & state)
+auto tParseFileSM =  [](benchmark::State & state, const std::string file,const std::string output_dir)
 {
     // Perform setup here
     for (auto _ : state) {
         // This code gets timed
-                std::string T = util::generate_random_string(LEN);
-                std::fstream fout("tmp",std::ios::out);
-                fout.write(T.c_str(),T.length());
-                fout.close();
-
-//                std::cout<<"TEXT:"<<std::endl;
-//                for (int i = 0; i < 10 ; ++i) {
-//                    std::cout<<T[i];
-//                }
-//                std::cout<<std::endl;
-
-
-
-                HashParserConfig<KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string>> conf(10,1,5,"tmp","");
+                HashParserConfig<KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string>> conf(10,1,5,file,output_dir);
                 conf.print();
                 HashParser< HashParserConfig< KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string> >> parser(conf);
 
                 parser.parseFileSM();
                 parser.results.print();
-                parser.recreateFile("tmp",1);
 
-               ASSERT_TRUE(util::compareFiles("tmp","tmp_recreated"));
-//                sleep(4);
+                std::string _f = file;
+                std::reverse(_f.begin(),_f.end());
+                int i = 0;
+                while(_f[i] != '/') ++i;
+
+                std::string filename;filename.resize(i+1);
+                std::copy(_f.begin(),_f.begin()+i,filename.begin());
+                std::reverse(filename.begin(),filename.end());
+                parser.recreateFile(output_dir+filename,1);
+
+               ASSERT_TRUE(util::compareFiles(file,output_dir+filename));
     }
+};
 
+int main (int argc, char *argv[] ){
+
+    std::string file = argv[1];
+    std::string output_dir = argv[2];
+
+    std::cout<<"file:"<<file<<std::endl;
+    std::cout<<"output_dir:"<<output_dir<<std::endl;
+
+
+    benchmark::RegisterBenchmark("hash-parser",tParseFileSM,file,output_dir)->Unit({benchmark::kMicrosecond});
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    return 0;
 
 }
-//
-//
-//static void tPrepareForRP(benchmark::State & state)
-//{
-//    // Perform setup here
-//    for (auto _ : state) {
-//        // This code gets timed
-//
-//
-//        std::string T = util::generate_random_string(100);
-//
-//        std::fstream fout("./tmp",std::ios::out);
-//        fout.write(T.c_str(),T.length());
-//        fout.close();
-//
-//
-//        HashParserConfig<KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string>> conf(10,1,10,"./tmp","./");
-//
-//        conf.print();
-//
-//        HashParser< HashParserConfig< KRPSlindingWindow<>,KRPHashFunction<uint64_t ,std::string> >> parser(conf);
-//
-//        parser.parseFile();
-//
-//        parser.results.print();
-//
-//        sleep(3);
-//
-//        util::prepareDiccFileForRP("file_dicc","file.dicc",sizeof(uint32_t),parser.results._max_alph_val);
-//
-//
-//
-//
-//    }
-//
-//}
-
-
-static void firstTest(benchmark::State & state)
-{
-    // Perform setup here
-    for (auto _ : state) {
-        // This code gets timed
-    }
-
-}
-// Register the function as a benchmark
-//BENCHMARK( firstTest)->Unit(benchmark::TimeUnit::kMicrosecond);
-BENCHMARK( tParseFileSM)->Unit(benchmark::TimeUnit::kMicrosecond);
-//BENCHMARK( t)->Args({0})->Unit(benchmark::TimeUnit::kMicrosecond);
-//BENCHMARK( tParseFile)->Unit(benchmark::TimeUnit::kMicrosecond);
-//BENCHMARK( tPrepareForRP)->Unit(benchmark::TimeUnit::kMicrosecond);
-
-// Run the benchmark
-BENCHMARK_MAIN();
